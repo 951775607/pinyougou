2b9f2f3f09797f271c34ecf2e3276d914cfd9382
Leon <951775607@qq.com>
2018-12-07 20:51:53 +0800

    1.按照价格区间筛选 2.搜索结果分页 3.多关键字搜索 4.排序 5.门户系统首页搜索跳转

--- pinyougou/pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou/pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -5,13 +5,16 @@ app.controller("searchController", function ($scope, searchService) {
 
     //添加过滤条件
     $scope.addSearchItem = function (key, value) {
-        if ("brand" == key || "category" == key) {
+        if ("brand" == key || "category" == key || "price" == key) {
+
             //如果点击的是品牌说着分类的话
             $scope.searchMap[key] = value;
         } else {
             //规格
             $scope.searchMap.spec[key] = value;
         }
+        $scope.searchMap.pageNo = 1;
+
         //点击过滤条件后重新搜索
         $scope.search();
     };
@@ -24,6 +27,7 @@ app.controller("searchController", function ($scope, searchService) {
             //删除规格对象中的属性
             delete $scope.searchMap.spec[key];
         }
+        $scope.searchMap.pageNo = 1;
         //查询
         $scope.search();
 
@@ -32,12 +36,104 @@ app.controller("searchController", function ($scope, searchService) {
 
     //搜索
     $scope.search = function () {
-
         searchService.search($scope.searchMap).success(function (reponse) {
             $scope.resultMap = reponse;
+            //构建页面分页导航条信息
+            buildPageInfo();
         });
-
     };
+
+    //构建页面分页导航条信息
+    buildPageInfo = function () {
+        //定义要在页面显示的页号的集合
+        $scope.pageNoList = [];
+
+        //定义要在页面显示的页号的数量
+        var showPageNoTotal = 5;
+
+        //起始页号
+        var startPageNo = 1;
+
+        //结束页号
+        var endPageNo = $scope.resultMap.totalPages;
+
+        //如果总页数大于要显示的总页号数才显示
+        if ($scope.resultMap.totalPages > showPageNoTotal) {
+
+            //当前页的左右间隔页数
+            var interval = Math.floor(showPageNoTotal / 2);
+
+            //起始页号
+            startPageNo = parseInt($scope.searchMap.pageNo) - interval;
+            //结束页号
+            endPageNo = parseInt($scope.searchMap.pageNo) + interval;
+
+            //如果结束页号大于总页数
+            if (endPageNo > $scope.resultMap.totalPages) {
+                startPageNo = $scope.resultMap.totalPages - showPageNoTotal + 1;
+                endPageNo = $scope.resultMap.totalPages;
+            } else if (startPageNo < 1) {
+                //如果起始页号小于则为1
+                startPageNo = 1;
+                endPageNo = showPageNoTotal;
+            }
+        }
+
+        //前面三个点
+        $scope.frontDot = false;
+        if (startPageNo > 1) {
+            $scope.frontDot = true;
+        }
+        //后面三个点
+        $scope.backDot = false;
+        if (endPageNo < $scope.resultMap.totalPages) {
+            $scope.backDot = true;
+        }
+
+        //设置要显示的页号
+        for (var i = startPageNo; i <= endPageNo; i++) {
+            $scope.pageNoList.push(i);
+        }
+    };
+
+    //判断是否为当前页
+    $scope.isCurrentPage = function (pageNo) {
+        return $scope.searchMap.pageNo == pageNo;
+    };
+
+    //根据页号查询
+    $scope.queryByPageNo = function (pageNo) {
+        if(0 < pageNo && (pageNo <= $scope.resultMap.totalPages)){
+            //设置页号
+            $scope.searchMap.pageNo = pageNo;
+
+            $scope.search();
+        }
+    };
+
+    //下一页
+    $scope.nextPage = function () {
+        $scope.queryByPageNo(parseInt($scope.searchMap.pageNo)+1);
+    };
+
+    //添加排序查询
+    $scope.sortSearch = function (sortField, sort) {
+        $scope.searchMap.sortField = sortField;
+        $scope.searchMap.sort = sort;
+
+        $scope.search();
+    };
+
+    //加载搜索关键字并搜索
+    $scope.loadKeywords = function () {
+
+        //获取到浏览器地址栏中的请求参数；地址 如：http://search.pinyougou.com/search.html#?keywords=小米
+        $scope.searchMap.keywords = $location.search()["keywords"];
+        $scope.search();
+    };
+
+
+
 
 });
 
